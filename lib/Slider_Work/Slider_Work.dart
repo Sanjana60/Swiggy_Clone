de98a0ca361a92a056d1c41f9ecaf708d3f63fe9
Sanjana60 <78793118+Sanjana60@users.noreply.github.com>
2023-04-13 17:00:51 +0530

    commit

--- lib/Slider_Work/Slider_Work.dart
+++ lib/Slider_Work/Slider_Work.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:carousel_slider/carousel_slider.dart';
+import 'package:dots_indicator/dots_indicator.dart';
 
 class Slider_Work extends StatefulWidget {
   @override
@@ -13,45 +14,63 @@ class _Slider_WorkState extends State<Slider_Work> {
     'images/3.jpeg',
     'images/4.jpeg'
   ];
+  double currentIndex = 0;
+  CarouselController buttonCarouselController = CarouselController();
   @override
   Widget build(BuildContext context) {
     return Container(
       margin: EdgeInsets.all(15),
-      child: CarouselSlider.builder(
-        itemCount: imageList.length,
-        options: CarouselOptions(
-          enlargeCenterPage: true,
-          height: 250,
-          autoPlay: true,
-          autoPlayInterval: Duration(seconds: 3),
-          reverse: false,
-          aspectRatio: 5.0,
-        ),
-        itemBuilder: (context, i, id) {
-          //for onTap to redirect to another screen
-          return GestureDetector(
-            child: Container(
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.circular(15),
-                  border: Border.all(
-                    color: Colors.white,
-                  )),
-              //ClipRRect for image border radius
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(15),
-                child: Image.network(
-                  imageList[i],
-                  width: 500,
-                  fit: BoxFit.cover,
-                ),
-              ),
+      child: Column(
+        children: [
+          CarouselSlider.builder(
+            carouselController: buttonCarouselController,
+            itemCount: imageList.length,
+            options: CarouselOptions(
+              onPageChanged: (i, reason) {
+                setState(() {
+                  currentIndex = i.toDouble();
+                });
+              },
+              enlargeCenterPage: true,
+              height: 250,
+              autoPlay: true,
+              autoPlayInterval: Duration(seconds: 3),
+              reverse: false,
+              aspectRatio: 5.0,
             ),
-            onTap: () {
-              var url = imageList[i];
-              print(url.toString());
+            itemBuilder: (context, i, id) {
+              //for onTap to redirect to another screen
+              return GestureDetector(
+                child: Container(
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(15),
+                      border: Border.all(
+                        color: Colors.white,
+                      )),
+                  //ClipRRect for image border radius
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(15),
+                    child: Image.network(
+                      imageList[i],
+                      width: 500,
+                      fit: BoxFit.fill,
+                    ),
+                  ),
+                ),
+              );
             },
-          );
-        },
+          ),
+          DotsIndicator(
+            dotsCount: imageList.length,
+            position: currentIndex,
+            decorator: DotsDecorator(
+              size: const Size.square(9.0),
+              activeSize: const Size(18.0, 9.0),
+              activeShape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(5.0)),
+            ),
+          ),
+        ],
       ),
     );
   }
